c899f0b9d079eaa54449a51b1ea5518ca58a949d
henkman <henkman@github.com>
2021-05-12 11:41:25 +0200

    make db work

--- db-service.js
+++ db-service.js
@@ -1,17 +1,30 @@
 const low = require("lowdb");
+const { nanoid } = require("nanoid")
 const FileSync = require("lowdb/adapters/FileSync");
 
 const adapter = new FileSync("db.json");
 const db = low(adapter);
 
-// Set some defaults
-db.defaults({ player: {} }).write();
-db.defaults({ match: {} }).write();
-db.set("player.uid", "110").write();
+db.defaults({
+  players: [],
+  match: {}
+}).write();
 
-const fetchPlayerById = function (uid) {
-  console.log(uid);
-  console.log(db.get("player").find({ uid: uid }).value());
+const players = {
+  getAll: () => {
+    return db.get("players")
+      .value();
+  },
+  getById: () => {
+    return db.get("players")
+      .find({ uid: uid })
+      .value();
+  },
+  create: (name) => {
+    return db.get('players')
+      .push({ uid: nanoid(12), name: name })
+      .write();
+  },
 };
 
-module.exports = { fetchPlayerById };
+module.exports = { players };
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const dbService = require("./db-service");
+const db = require("./db-service");
 
 const app = express();
 const port = 3000;
@@ -12,4 +12,7 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
-console.log(dbService.fetchPlayerById("110"));
+
+db.players.create("dirk");
+
+console.log(db.players.getAll());
