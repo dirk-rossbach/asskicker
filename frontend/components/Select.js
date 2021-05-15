e19a49b6815b189c31ba1fb353d0f6539ddd6e7d
Max Eise <Maximilian.eise@gmail.com>
2021-05-15 16:17:57 +0200

    Update new match page

--- frontend/components/Select.js
+++ frontend/components/Select.js
@@ -7,17 +7,25 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
-export default function Select({ players }) {
+export default function Select({ players, number }) {
   const people = players;
-  const [selected, setSelected] = useState(people[3]);
+  const [selected, setSelected] = useState(people[0]);
+  const player_number = number;
+  // console.log(selected);
+
+  const selection = [];
+
+  function changeHandler() {
+    console.log(selected);
+  }
 
   return (
     <Listbox value={selected} onChange={setSelected}>
       {({ open }) => (
         <>
-          <Listbox.Label className="block text-sm font-medium text-gray-700">Player 1</Listbox.Label>
+          <Listbox.Label className="block text-sm font-medium text-gray-700">Player {player_number}</Listbox.Label>
           <div className="mt-1 relative">
-            <Listbox.Button className="relative w-full bg-white border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+            <Listbox.Button className="relative w-full bg-white border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-vita-500 focus:border-vita-500 sm:text-sm">
               <span className="flex items-center">
                 <img src={selected.avatar} alt="" className="flex-shrink-0 h-6 w-6 rounded-full" />
                 <span className="ml-3 block truncate">{selected.name}</span>
@@ -35,7 +43,7 @@ export default function Select({ players }) {
                 {people.map((person) => (
                   <Listbox.Option
                     key={person.id}
-                    className={({ active }) => classNames(active ? "text-white bg-indigo-600" : "text-gray-900", "cursor-default select-none relative py-2 pl-3 pr-9")}
+                    className={({ active }) => classNames(active ? "text-white bg-vita-600" : "text-gray-900", "cursor-default select-none relative py-2 pl-3 pr-9")}
                     value={person}
                   >
                     {({ selected, active }) => (
@@ -46,7 +54,7 @@ export default function Select({ players }) {
                         </div>
 
                         {selected ? (
-                          <span className={classNames(active ? "text-white" : "text-indigo-600", "absolute inset-y-0 right-0 flex items-center pr-4")}>
+                          <span className={classNames(active ? "text-white" : "text-vita-600", "absolute inset-y-0 right-0 flex items-center pr-4")}>
                             <CheckIcon className="h-5 w-5" aria-hidden="true" />
                           </span>
                         ) : null}
