7d41bdaa9bc45a38dee0f53d19f76059683027c3
Dirk <>
2021-05-19 15:06:55 +0200

    help me I'm trapped in a component

--- retro frontend/src/components/SelectPlayers/PlayerCell.js
+++ retro frontend/src/components/SelectPlayers/PlayerCell.js
@@ -3,11 +3,13 @@ import "./playercell.scss";
 
 export default class PlayerCell extends Component {
   render() {
-    const classes = this.props.active === true ? "active playercell" : "playercell";
+    const classes =
+      this.props.active === true ? "active playercell" : "playercell";
     return (
       <span className={classes}>
+        <span className="indicator">&lt;</span>
+        <span className="playerID">{this.props.player.id}</span>
         <span className="indicator">&gt;</span>
-        {this.props.player.id}
       </span>
     );
   }
--- retro frontend/src/components/SelectPlayers/PlayerMatrix.js
+++ retro frontend/src/components/SelectPlayers/PlayerMatrix.js
@@ -5,25 +5,67 @@ import "./playermatrix.scss";
 export default class PlayerMatrix extends Component {
   constructor(props) {
     super(props);
-    this.state = { active: 2 };
+    this.textInput = null;
+    this.state = {
+      active: 0,
+      players: this.props.players,
+    };
   }
+  setTextInputRef = (element) => {
+    this.textInput = element;
+  };
+
   handleKeyPress = (e) => {
+    //LEFT
+    if (e.keyCode === 37) {
+      this.props.teamChanged({
+        team0: document.querySelector(".active.playercell .playerID")
+          .textContent,
+      });
+    }
+    //UP
     if (e.keyCode === 38) {
       if (this.state.active === 0) {
         return;
       }
       this.setState({ active: this.state.active - 1 });
-    } else if (e.keyCode === 40) {
-      if (this.state.active === this.props.players.length - 1) {
+    }
+    //RIGHT
+    if (e.keyCode === 39) {
+      this.props.teamChanged({
+        team1: document.querySelector(".active.playercell .playerID")
+          .textContent,
+      });
+    }
+    //DOWN
+    if (e.keyCode === 40) {
+      if (this.state.active === this.state.players.length - 1) {
         return;
       }
       this.setState({ active: this.state.active + 1 });
     }
   };
 
+  componentDidUpdate(prevProps) {
+    if (prevProps.players !== this.props.players) {
+      this.setState({ players: this.props.players });
+    }
+  }
+  handleChange = () => {
+    this.setState({ active: 0 });
+    this.filterPlayers();
+  };
+
+  filterPlayers() {
+    const filtered = this.props.players.filter((player) =>
+      player.id.toLowerCase().startsWith(this.textInput.value.toLowerCase())
+    );
+    this.setState({ players: filtered });
+  }
+
   render() {
     const players = [];
-    this.props.players.forEach((player, index) => {
+    this.state.players.forEach((player, index) => {
       players.push(
         <PlayerCell
           active={index === this.state.active}
@@ -33,12 +75,19 @@ export default class PlayerMatrix extends Component {
       );
     });
     return (
-      <div
-        tabIndex="0"
-        className="playermatrix"
-        onKeyDown={this.handleKeyPress}
-      >
-        {players}
+      <div>
+        <input
+          maxLength="3"
+          ref={this.setTextInputRef}
+          onChange={this.handleChange}
+        ></input>
+        <div
+          tabIndex="0"
+          className="playermatrix"
+          onKeyDown={this.handleKeyPress}
+        >
+          {players}
+        </div>
       </div>
     );
   }
