4780804cc2bf8dc1ac00197759b35431468c20a2
Max Eise <Maximilian.eise@gmail.com>
2021-07-18 22:38:03 +0200

    Websocket => Matchscreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,31 +7,18 @@ import MatchScreen from "./screens/MatchScreen/MatchScreen";
 
 import { useHistory, BrowserRouter as Router, Switch, Route, Redirect, Link } from "react-router-dom";
 
-import { w3cwebsocket as W3CWebSocket } from "websocket";
-
-const client = new W3CWebSocket("ws://localhost:3000/score");
-
 class App extends Component {
-  /* Connect Websocket with Backend */
-  componentWillMount() {
-    client.onopen = () => {
-      console.log("WebSocket Client Connected");
-    };
-    client.onmessage = (message) => {
-      console.log(message);
-    };
-  }
-
   render() {
     return (
       <Router>
         {/* Start & Redirect */}
+        {/*
         <Switch>
           <Route path="/">
             <Redirect exact from="/" to="/start" />
           </Route>
         </Switch>
-
+        */}
         {/* Start Screen */}
         <Switch>
           <Route path="/start">
