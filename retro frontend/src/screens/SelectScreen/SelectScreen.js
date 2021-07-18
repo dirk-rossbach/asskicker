e55005aa42f531764ba03810ab158d819a168266
Max Eise <Maximilian.eise@gmail.com>
2021-07-18 14:59:48 +0200

    SelectScreen working again

--- retro frontend/src/screens/SelectScreen/SelectScreen.js
+++ retro frontend/src/screens/SelectScreen/SelectScreen.js
@@ -1,18 +1,27 @@
 import React, { Component } from "react";
 import "./selectscreen.scss";
 
+import axios from "axios";
+
 import SelectPlayers from "../../components/SelectPlayers/SelectPlayers";
 import RetroButton from "../../components/RetroButton/Retrobutton";
+import Background from "../../components/Background/Background";
 
 class SelectScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { players: [] };
+  }
+  componentDidMount() {
+    axios.get("http://localhost:3000/roster/players").then((res) => {
+      this.setState({ players: res.data });
+    });
+  }
   render() {
     return (
       <div class="select_wrapper">
         <SelectPlayers players={this.state.players}></SelectPlayers>
-        <RetroButton color="red" text="foo"></RetroButton>
-        <RetroButton color="green" text="foo"></RetroButton>
-        <RetroButton color="blue" text="foo"></RetroButton>
-        <RetroButton text="clicky"></RetroButton>
+        <Background />
       </div>
     );
   }
